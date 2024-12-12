94495b6d9c0a0991cab427d84b03d675ff32d8c3
AndrejZaf <andrej.zafirovski@students.finki.ukim.mk>
2024-12-13 00:40:54 +0100

    Finalize the navigation bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,77 +1,105 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { MenuIcon } from "lucide-react";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { BadgeCheck, LogOut, MenuIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
+const navigation = [
+    {
+        href: "/home",
+        label: "Home"
+    },
+    {
+        href: "/recipes",
+        label: "Recipes"
+    },
+    {
+        href: "/explore",
+        label: "Explore"
+    },
+    {
+        href: "/dashboard",
+        label: "Dashboard"
+    }
+];
+
 const Navbar = () => {
     return (
-        <div className="flex items-center justify-between px-4  bg-[#627AF7] dark:bg-gray-800">
-            <Link href="#" className="flex items-center gap-2" prefetch={false}>
-                <img alt="Whac a meal" src="./whc_logo.svg" />
-            </Link>
-            <div className="hidden md:flex gap-4 text-white uppercase">
-                <Link href="#" className="text-lg py-2 font-semibold hover:underline underline-offset-4" prefetch={false}>
-                    Home
-                </Link>
-                <Link href="#" className="text-lg py-2 font-semibold hover:underline underline-offset-4" prefetch={false}>
-                    About
-                </Link>
-                <Link href="#" className="text-lg py-2 font-semibold hover:underline underline-offset-4" prefetch={false}>
-                    Services
-                </Link>
-                <Link href="#" className="text-lg py-2 font-semibold hover:underline underline-offset-4" prefetch={false}>
-                    Portfolio
-                </Link>
-                <Link href="#" className="text-lg py-2 font-semibold hover:underline underline-offset-4" prefetch={false}>
-                    Contact
-                </Link>
-            </div>
-            <div className="hidden md:flex">
-                <Avatar>
-                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-            </div>
+        <div className="flex items-center justify-between px-4 py-2 md:py-0 bg-[#627AF7] dark:bg-gray-800">
             <Sheet>
                 <SheetTrigger asChild>
-                    <Button variant="outline" size="icon" className="md:hidden">
-                        <MenuIcon className="h-6 w-6" />
+                    <Button variant="outline" size="icon" className="md:hidden bg-[#627AF7]">
+                        <MenuIcon className="text-white h-6 w-6" />
                         <span className="sr-only">Toggle navigation menu</span>
                     </Button>
                 </SheetTrigger>
-                <SheetContent side="left">
-                    <div className="grid w-[200px] p-4">
-                        <Link href="#" className="text-lg font-medium hover:underline underline-offset-4"
-                              prefetch={false}>
-                            Home
-                        </Link>
-                        <Link href="#" className="text-lg font-medium hover:underline underline-offset-4"
-                              prefetch={false}>
-                            About
-                        </Link>
-                        <Link href="#" className="text-lg font-medium hover:underline underline-offset-4"
-                              prefetch={false}>
-                            Services
-                        </Link>
-                        <Link href="#" className="text-lg font-medium hover:underline underline-offset-4"
-                              prefetch={false}>
-                            Portfolio
-                        </Link>
-                        <Link href="#" className="text-lg font-medium hover:underline underline-offset-4"
-                              prefetch={false}>
-                            Contact
-                        </Link>
-                        <div>
-                            <Avatar>
-                                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                                <AvatarFallback>CN</AvatarFallback>
-                            </Avatar>
-                        </div>
+                <SheetContent className="bg-[#627AF7]" side="left">
+                    <VisuallyHidden>
+                        <SheetHeader>
+                            <SheetTitle>Edit profile</SheetTitle>
+                            <SheetDescription>
+                                Make changes to your profile here. Click save when you're done.
+                            </SheetDescription>
+                        </SheetHeader>
+                    </VisuallyHidden>
+                    <div className="grid p-4">
+                        {navigation.map((item) =>
+                            <Link key={item.label} href={item.href}
+                                  className="transition-all delay-75 text-white uppercase text-lg py-2 font-semibold border-b-4 border-transparent hover:border-white">
+                                <div>
+                                    {item.label}
+                                </div>
+                            </Link>)}
                     </div>
                 </SheetContent>
             </Sheet>
+            <Link href="/" className="flex items-center gap-2">
+                <img alt="Whac a meal" src="./whc_logo.svg" />
+            </Link>
+            <div className="hidden md:flex gap-4">
+                {navigation.map((item) =>
+                    <Link key={item.label} href={item.href}
+                          className="transition-all delay-75 text-white uppercase text-lg py-2 font-semibold border-b-4 border-transparent hover:border-white">
+                        <div>
+                            {item.label}
+                        </div>
+                    </Link>)}
+            </div>
+            <div className="flex">
+                <DropdownMenu modal={false}>
+                    <DropdownMenuTrigger asChild>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent
+                        className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                        side="bottom"
+                        align="end"
+                        sideOffset={4}
+                    >
+                        <DropdownMenuItem>
+                            <BadgeCheck />
+                            Account
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>
+                            <LogOut />
+                            Log out
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
         </div>
     );
 };
