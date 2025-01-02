a49f0a0ecab235fc2dff0885c33b8f8404ccce55
AndrejZaf <andrej.zafirovski@students.finki.ukim.mk>
2025-01-02 23:30:43 +0100

    Fix the parallel route if the dialog is closed

--- app/@auth/(.)login/page.tsx
+++ app/@auth/(.)login/page.tsx
@@ -2,16 +2,21 @@
 
 import LoginForm from "@/components/auth/login-form";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const LoginInterceptor = () => {
     const [open, setOpen] = useState(false);
+    const router = useRouter();
     useEffect(() => {
         setOpen(true);
     }, []);
 
     return (
-        <Dialog open={open} onOpenChange={setOpen}>
+        <Dialog open={open} onOpenChange={(value) => {
+            router.back();
+            setOpen(value);
+        }}>
             <DialogContent className="sm:max-w-sm">
                 <DialogHeader>
                     <DialogTitle>Login</DialogTitle>
