53d634dc563f06982601355265c62efb91b87ed9
AndrejZaf <andrej.zafirovski@students.finki.ukim.mk>
2025-01-03 23:26:06 +0100

    Create a static component for the auth split screen and proceed to work on the parallel route fix

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,4 @@
+import AuthSidebar from "@/components/auth-sidebar";
 import LoginForm from "@/components/auth/login-form";
 import Link from "next/link";
 import React from "react";
@@ -6,22 +7,7 @@ const LoginPage = () => {
     return (
         <div
             className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-            <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-                <div className="absolute inset-0 bg-zinc-900" />
-                <div className="relative z-20 flex items-center text-lg font-medium">
-                    <img alt="Whac a meal" src="./whc_logo.svg" />
-                </div>
-                <div className="relative z-20 mt-auto">
-                    <blockquote className="space-y-2">
-                        <p className="text-lg">
-                            &ldquo;This library has saved me countless hours of work and
-                            helped me deliver stunning designs to my clients faster than
-                            ever before.&rdquo;
-                        </p>
-                        <footer className="text-sm">Sofia Davis</footer>
-                    </blockquote>
-                </div>
-            </div>
+            <AuthSidebar />
             <div className="lg:p-8">
                 <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                     <div className="flex flex-col space-y-2 text-center">
