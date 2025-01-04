c96b562c2d8682f9789f82881d3d910792c15d19
AndrejZaf <andrej.zafirovski@students.finki.ukim.mk>
2025-01-05 00:51:49 +0100

    Improve mobile responsiveness in the login and signup screen

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -6,10 +6,10 @@ import React from "react";
 const LoginPage = () => {
     return (
         <div
-            className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+            className="container relative h-screen flex-col items-center justify-center mx-auto md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
             <AuthSidebar />
-            <div className="lg:p-8">
-                <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex justify-center align-middle lg:p-8 h-screen">
+                <div className="mx-auto flex flex-col justify-center space-y-6 max-w-sm">
                     <div className="flex flex-col space-y-2 text-center">
                         <h1 className="text-2xl font-semibold tracking-tight">
                             Create an account
