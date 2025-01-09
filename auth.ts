c98153916ed90d692447ec33fb53c8a091b89ad7
AndrejZaf <andrej.zafirovski@students.finki.ukim.mk>
2025-01-09 10:29:11 +0100

    Resolve the issue with node-postgres and fix the crypto next auth compatibility issue

--- auth.ts
+++ auth.ts
@@ -1,8 +1,10 @@
+import authConfig from "@/auth.config";
 import { db } from "@/db";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: DrizzleAdapter(db),
-    providers: []
+    session: { strategy: "jwt" },
+    ...authConfig
 });
--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,20 @@
-export { auth as middleware } from "@/auth";
+// export { auth as middleware } from "@/auth";
+
+import authConfig from "@/auth.config";
+import NextAuth from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth(async function middleware(req: NextRequest) {
+    return NextResponse.next();
+});
+
+export const config = {
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+        // Always run for API routes
+        "/(api|trpc)(.*)"
+    ]
+};
