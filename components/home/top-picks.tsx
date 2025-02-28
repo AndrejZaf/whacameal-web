bea1839e3959e64e9734244ec93c59d3f755f058
AndrejZaf <andrej.zafirovski@students.finki.ukim.mk>
2025-02-28 23:38:43 +0100

    Finalize the base recipe cards

--- components/home/top-picks.tsx
+++ components/home/top-picks.tsx
@@ -5,9 +5,12 @@ import React from "react";
 
 const TopPicks = () => {
     return (
-        <div className="container mx-auto">
+        <div className="container mx-auto space-y-6">
             <div className="text-center text-3xl font-semibold">Our Top Picks</div>
-            <div className="cards">
+            <div className="cards grid grid-cols-1 px-4 md:px-0 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+                <RecipeCard />
+                <RecipeCard />
+                <RecipeCard />
                 <RecipeCard />
             </div>
             <div className="text-center">
