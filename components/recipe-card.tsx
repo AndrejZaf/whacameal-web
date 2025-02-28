27796d56b53f602943cbac3dbaf182dca3283a6c
AndrejZaf <andrej.zafirovski@students.finki.ukim.mk>
2025-02-28 19:30:51 +0100

    Align the recipe card to the figma design

--- components/recipe-card.tsx
+++ components/recipe-card.tsx
@@ -1,18 +1,18 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 import React from "react";
 
 const RecipeCard = () => {
     return (
-        <Card>
-            <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
+        <Card className="aspect-square">
+            <CardHeader className="p-0">
+                <img className="rounded-t-lg" src="./cover.webp" />
             </CardHeader>
-            <CardContent>
-                <p>Card Content</p>
+            <CardContent className="text-xl font-bold">
+                Double Smash Cheese Burger
             </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
+            <CardFooter className="flex justify-between text-[#6B6B6B]">
+                <div>20 minutes</div>
+                <div>Gluten</div>
             </CardFooter>
         </Card>
     );
