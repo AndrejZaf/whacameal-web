3cc25958b75be32884eda9711d86cd5cf30dafd9
AndrejZaf <andrej.zafirovski@students.finki.ukim.mk>
2025-08-06 11:41:01 +0200

    Generate a new migration file and fix the remaining issue in the recipe schema

--- db/schema/recipe.ts
+++ db/schema/recipe.ts
@@ -1,30 +1,41 @@
-import { users } from "@/db/schema/user";
-import { AnyPgColumn, integer, pgEnum, pgTable, text } from "drizzle-orm/pg-core";
+import { user } from "@/db/schema/user";
+import {
+  AnyPgColumn,
+  integer,
+  pgEnum,
+  pgTable,
+  text,
+} from "drizzle-orm/pg-core";
 
-export const mealType = pgEnum("meal_type", ["BREAKFAST", "BRUNCH", "LUNCH", "DINNER", "SNACK", "LATE_NIGHT", "HOLIDAY"]);
-export const mealCourse = pgEnum("meal_course", ["APPETIZER", "SOUP", "SALAD", "MAIN_COURSE", "SIDE_DISH", "DESSERT"]);
-export const recipes = pgTable(
-    "recipe",
-    {
-        id: text("id")
-            .primaryKey()
-            .$defaultFn(() => crypto.randomUUID()),
-        userId: text("user_id")
-            .references((): AnyPgColumn => users.id)
-            .notNull(),
-        title: text("title")
-            .notNull(),
-        prepTime: integer("prep_time")
-            .notNull(),
-        cookTime: integer("cook_time")
-            .notNull(),
-        servings: integer("servings")
-            .notNull(),
-        instructions: text("instructions")
-            .notNull(),
-        mealType: mealType("meal_type")
-            .notNull(),
-        mealCourse: mealCourse("meal_course")
-            .notNull(),
-    },
-);
+export const mealType = pgEnum("meal_type", [
+  "BREAKFAST",
+  "BRUNCH",
+  "LUNCH",
+  "DINNER",
+  "SNACK",
+  "LATE_NIGHT",
+  "HOLIDAY",
+]);
+export const mealCourse = pgEnum("meal_course", [
+  "APPETIZER",
+  "SOUP",
+  "SALAD",
+  "MAIN_COURSE",
+  "SIDE_DISH",
+  "DESSERT",
+]);
+export const recipes = pgTable("recipe", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  userId: text("user_id")
+    .references((): AnyPgColumn => user.id)
+    .notNull(),
+  title: text("title").notNull(),
+  prepTime: integer("prep_time").notNull(),
+  cookTime: integer("cook_time").notNull(),
+  servings: integer("servings").notNull(),
+  instructions: text("instructions").notNull(),
+  mealType: mealType("meal_type").notNull(),
+  mealCourse: mealCourse("meal_course").notNull(),
+});
