cec5628a9dab0c5e5080f501ab20cccd64e1f12f
AndrejZaf <andrej.zafirovski@students.finki.ukim.mk>
2025-08-08 07:48:41 +0200

    Create a base dialog component with static fields

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -1,10 +1,11 @@
+import RecipeDialog from "@/components/dashboard/recipe-dialog";
 import { Button } from "@/components/ui/button";
 
 const DashboardPage = () => {
   return (
     <>
       <div className="flex justify-end">
-        <Button>Create Recipe</Button>
+        <RecipeDialog />
       </div>
     </>
   );
